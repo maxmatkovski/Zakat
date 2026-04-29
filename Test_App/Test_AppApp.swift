191ba7d39b179e78f44c6d576c747e6c35d66a88
Max Matkovski <maxmatkovski@gmail.com>
2026-04-29 18:10:15 +0300

    Initial Commit

---
+++ Test_App/Test_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_AppApp.swift
+//  Test_App
+//
+//  Created by Max Matkovski on 29/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct Test_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
